7a498b117955b561c29814b83ae16617e110a3ef
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2024-10-16 16:57:09 +0300

    fix deploy

--- src/lib/ymaps.ts
+++ src/lib/ymaps.ts
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const [ymaps3React] = await Promise.all([
-  ymaps3.import("@yandex/ymaps3-reactify"),
-  ymaps3.ready,
-]);
+const init = async () => {
+  const [ymaps3React] = await Promise.all([
+    ymaps3.import("@yandex/ymaps3-reactify"),
+    ymaps3.ready,
+  ]);
+  return ymaps3React;
+};
+const ymaps3React = await init();
 
 export const reactify = ymaps3React.reactify.bindTo(React, ReactDom);
 export const {
